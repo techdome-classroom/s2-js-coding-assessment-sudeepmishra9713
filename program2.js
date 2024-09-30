154dd3598e7ae31054460a1a86215c3af23e364a
sudeepmishra9713 <mishrasudeep509@gmail.com>
2024-09-30 14:04:58 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -3,8 +3,21 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    
+    let mapping = {I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000};
+    let result = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        let current = mapping[s[i]];
+        let next = i + 1 < s.length ? mapping[s[i + 1]] : 0;
+
+        if (current < next) {
+            result -= current;
+        } else {
+            result += current;
+        }
+    }
+
+    return result;
 };
 
-
-module.exports={romanToInt}
+module.exports = { romanToInt };
