4b27b119a60215275074c856186ba12c29c16ad4
sudeepmishra9713 <mishrasudeep509@gmail.com>
2024-09-30 14:02:44 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -3,7 +3,20 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    let stack = [];
+    let mapping = {")": "(", "}": "{", "]": "["};
+
+    for (let char of s) {
+        if (char === "(" || char === "{" || char === "[") {
+            stack.push(char);
+        } else if (char === ")" || char === "}" || char === "]") {
+            if (stack.length === 0 || mapping[char] !== stack.pop()) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
